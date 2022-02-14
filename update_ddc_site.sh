6f2091932755871299b1fd1a7e68ba73f8fda90e
Mike Lake <Mike.Lake@uts.edu.au>
2022-02-14 16:04:04 +1100

    Added example script on how to create a web site

---
+++ update_ddc_site.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Process several directories and output Markdown docs.
+# Then create a static website of those docs using the 
+# static website generator MkDocs and upload the site.
+#
+# Usage: 
+# Make sure you are in this git repo directory then run:
+#   ./update_ddc_site.sh
+
+# Catalog the README.yaml files in the folloing directories.
+sudo ./ddc.py /shared                       > docs/shared.md
+./ddc.py /shared/eresearch                  > docs/shared_eresearch.md
+./ddc.py /shared/eresearch/pbs_job_examples > docs/shared_eresearch_pbs_job_examples.md
+./ddc.py /shared/opt/                       > docs/shared_opt.md
+
+# Build the website.
+source ~/virtualenvs/mkdocs3/bin/activate
+mkdocs build
+
+# If you wish to use --delete do not use "site/*" use "site/". Man rsync for why.
+rsync -r --delete site/* /var/www/html_ddc/ 
+
