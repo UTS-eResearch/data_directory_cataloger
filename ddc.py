b6cdad60f17f8a27661f029e48733ef0fc3425d8
Mike Lake <Mike.Lake@uts.edu.au>
2022-02-15 14:17:29 +1100

    Changed copyright

--- ddc.py
+++ ddc.py
@@ -23,11 +23,11 @@ License
 
 Copyright 2022 Mike Lake
 
-Data Directory Cataloger is free software: you can redistribute it and/or modify it
+This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later version.
 
-Data Directory Cataloger is distributed in the hope that it will be useful, but
+This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 details.
