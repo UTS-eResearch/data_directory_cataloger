e262bbaf944ecbce5ef896f8dd992f63496e2e64
Mike Lake <MikeL@speleonics.com.au>
2022-03-07 23:46:05 +1100

    Changed test to example.

--- run_markdown.sh
+++ run_markdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-input="test.md"
-output="test.html"
+input="example.md"
+output="example.html"
 
 # This requires pandoc which is installed on the HPC.
 # It also requires a styles.css to be in this directory.
@@ -15,9 +15,9 @@ output="test.html"
 
 # This will produce a standalone HTML doc with head and footer.
 # The --self-contained option will insert the styles into the head of the HTML.
-# Hence it will not require the separate styles.css file.
+# Hence it will not require the separate styles.css file to be present.
 pandoc --css=styles.css --self-contained $input > $output
 
 # Copy to our document root.
-cp $output /var/www/html/
+#cp $output /var/www/html/
 
