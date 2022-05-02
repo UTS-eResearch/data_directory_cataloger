a4a9e0996c0dcfbb23b56175aca5c9ff585a3219
Mike Lake <Mike.Lake@uts.edu.au>
2022-05-02 11:15:47 +1000

    Added site script for C3

---
+++ update_ddc_site_c3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Process several directories and output Markdown docs.
+# Then create a static website of those docs using the 
+# static website generator MkDocs and upload the site.
+#
+# Usage: 
+# Make sure you are in this repo directory then run:
+#   ./update_ddc_site.sh
+
+# Catalog the README.yaml files in the following directories.
+# sudo is needed here to search some directories under /shared.
+sudo true
+./ddc.py /shared                            > docs/shared.md
+./ddc.py /shared/eresearch                  > docs/shared_eresearch.md
+./ddc.py /shared/eresearch/pbs_job_examples > docs/shared_eresearch_pbs_job_examples.md
+./ddc.py /shared/opt                        > docs/shared_opt.md
+./ddc.py /shared/c3                         > docs/shared_c3.md
+
+# Build the website.
+source /shared/homes/mlake/virtualenvs/mkdocs3/bin/activate
+mkdocs build
+
+# If you wish to use --delete do not use "site/*" use "site/". Man rsync for why.
+rsync -r --delete site/ /var/www/html_ddc/ 
+
