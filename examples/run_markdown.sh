50a50dc66069865470d583240f3c0f04acbca3c2
Mike Lake <Mike.Lake@uts.edu.au>
2022-05-02 16:37:35 +1000

    Simplified

--- examples/run_markdown.sh
+++ examples/run_markdown.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
+# This takes the example.md Markdown document and converts it to a HTML page.
+# This requires pandoc. It also requires a styles.css to be in this directory.
+
 input="example.md"
 output="example.html"
+site="$HOME/public_html/ddc/"
 
-# This requires pandoc which is installed on the HPC.
-# It also requires a styles.css to be in this directory.
-
-# This will produce a fragment only i.e. no HTML head or footer.
-#pandoc --css=styles.css $input > $output
-
-# This will produce a standalone i.e. full HTML doc with head and footer as it uses the -s option.
-# It will require the separate styles.css file.
-#pandoc --css=styles.css --standalone $input > $output
-
-# This will produce a standalone HTML doc with head and footer.
-# The --self-contained option will insert the styles into the head of the HTML.
+# This will produce a standalone HTML doc.
+# The --self-contained option will insert the styles into the head of the doc.
 # Hence it will not require the separate styles.css file to be present.
 pandoc --css=styles.css --self-contained $input > $output
 
 # Copy to our document root.
-#cp $output /var/www/html/
+if [ -d $site ]; then 
+    cp $output $site
+else
+    echo "Unable to copy $output to $site as this directory was not found."
+fi
 
