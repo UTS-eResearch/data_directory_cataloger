0da118f6f351de44d27f42636dc0d339d9536ccb
Mike Lake <Mike.Lake@uts.edu.au>
2022-08-08 14:59:33 +1000

    Fix license

--- ddc.py
+++ ddc.py
@@ -25,17 +25,16 @@ License
 
 Copyright 2022 Mike Lake
 
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later version.
+This program is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Foobar.
-If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with this
+program. If not, see <https://www.gnu.org/licenses/>.
 '''
 
 # Filename of the README in each directory that contains the directory meta information.
