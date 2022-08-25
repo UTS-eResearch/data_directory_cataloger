8fe0721da15e2a5faef373bf320ef714496d0d36
Mike Lake <Mike.Lake@uts.edu.au>
2022-08-25 16:41:56 +1000

    Now displays warnings of invalid characters.

--- ddc.py
+++ ddc.py
@@ -132,12 +132,13 @@ def sanitise_data(data):
     could still insert javascript into the YAML files. Here we look for several chars
     that might indicate javascript content and replace or strip them out. This might
     cause some inconvenience to users.
-    TODO If characters are replaced tell the user in the "Warnings" section.
     '''
 
     # The set of characters to be replaced needs to be a dictionary.
     # For the key specify the single character to be replaced, and
     # for its value specify its replacement character. This can be None.
+    # This can be printed as a string with:
+    # print('Disllowed characters are: ', ' '.join(deny_list.keys()))
     deny_list = {
         '<':'&lt;',
         '>':'&gt;',
@@ -147,19 +148,22 @@ def sanitise_data(data):
         ')':'',
         ';':'' }
 
+    warnings = set()
+
     # str.maketrans takes a dictionary. For the key specify the single character
     # to be replaced, and for its value specify its replacement. 
     # It returns a translation table usable for str.translate().
     trans_table = str.maketrans(deny_list)
 
-    for path in data.keys():
+    for directory in data.keys():
         # We don't need to check the keys in the data dictionary as they
         # are the directory paths, found and entered by this program.
         # The values in the data dictionary are the YAML docs, each one being a
         # dictionary. We have to check theirs keys and values.
 
-        doc = data[path]  # Each doc is a single row in the Markdown table.
+        doc = data[directory]  # Each doc is a single row in the Markdown table.
         sanitised = False
+        path = os.path.join(directory, readme)
 
         # We have to make a shallow copy, otherwise we get a RuntimeError;
         # "dictionary changed size during iteration".
@@ -168,13 +172,13 @@ def sanitise_data(data):
         for key in doc_copy.keys():
             value = doc[key]
             # We do have to check all the keys and values in each "doc"
-            # TODO Can this can be turned into a list comprehension using a small function?
+            # TODO Can this can be simplified into a list comprehension using a small function?
             key_sanitised = key.translate(trans_table)
             if key_sanitised != key:
                 doc[key_sanitised] = value  # Insert a new key, with old keys value.
                 doc.pop(key)                # Remove the old key.
                 sanitised = True
-                #print('WARNING KEY REPLACED: ', path, ' '.join(deny_list.keys()), '    ')
+                warnings.add(path)   # Append directory only, not key.
                 if DEBUG:
                     print('DEBUG KEY REPLACED: ', key, '==>', key_sanitised)
 
@@ -186,14 +190,14 @@ def sanitise_data(data):
                 if value_sanitised != value:
                     doc[key] = value_sanitised
                     sanitised = True
-                    #print('WARNING VALUE REPLACED: ', path, ' '.join(deny_list.keys()), '    ')
+                    warnings.add(path)  # Append directory only, not value.
                     if DEBUG:
                         print('DEBUG VALUE REPLACED: ', value, ' ==> ', value_sanitised)
 
         if sanitised:
-            data[path] = doc
-
-    return data
+            data[directory] = doc
+   
+    return (data, warnings)
 
 def get_metadata(data):
     '''
@@ -221,15 +225,17 @@ def print_metadata(metadata):
     Print the metadata as a Markdown list.
     '''
 
+    print('\n## Metadata Information')
     print('\nThe following are all the %d metadata attributes found in the %s files at this level.' \
         % (len(metadata), readme))
     print('These should all be unique. If not edit and correct the %s files.' % readme)
     print('')
     [print(' -', item) for item in sorted(metadata)]
 
-def check_metadata(data, metadata):
+def check_metadata(data, metadata, warnings):
     '''
-    Check if any READMEs are missing any metadata values.
+    Check if any READMEs are missing any metadata values or
+    if any keys or values contain disallowed characters.
     '''
 
     passed = True
@@ -266,6 +272,12 @@ def check_metadata(data, metadata):
     if passed:
         print('\nChecking each %s file against the metadata list above ... OK' % readme)
 
+    if len(warnings):
+        print('\nChecking each README.yaml file for disallowed characters ...')
+        #print(' '.join(deny_list.keys()))
+        print('')
+        [print(' -', item) for item in warnings]
+    
 def create_markdown_header(timenow):
     '''
     Valid Markdown docs have some metadata at the top. Set that here.
@@ -323,12 +335,12 @@ def create_markdown_table(columns, data):
     print(' |')
 
     # Print the data.
-    for key in data.keys():
-        doc = data[key]
+    for directory in data.keys():
+        doc = data[directory]
         # Remember that 'Directory' is the first in our list of columns to print
         # and the value of this is the key in the data dictionary. But the actual
         # docs don't contain a value for this so we add this now.
-        doc['Directory'] = key
+        doc['Directory'] = directory
         l = []
         for col in columns:
             if col not in doc:
@@ -384,7 +396,7 @@ def main():
     data = parse_readmes(found)
 
     # Sanitise the data as this is user input.
-    data = sanitise_data(data)
+    (data, warnings) = sanitise_data(data)
 
     print('A summary of the metadata in these files follows.')
     # Place in this list the keys that you wish to print out.
@@ -392,12 +404,11 @@ def main():
     columns = ['Title', 'Description', 'Data Manager']
     create_markdown_table(columns, data)
 
-    print('\n## Metadata Information')
     metadata = get_metadata(data)
     print_metadata(metadata)
 
     # Check the metadata in these READMEs for consistency.
-    check_metadata(data, metadata)
+    check_metadata(data, metadata, warnings)
 
     create_markdown_footer(version, timenow)
 
